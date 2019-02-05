27c90cff5edc9df0b78b361b5e2bfabcc7f55a4d
Lukaskov <sabin6341@gmail.com>
2019-02-05 17:18:01 +0200

    add middleware

---
+++ db/Petition.js
@@ -0,0 +1,18 @@
+const potition = [
+    {
+        id:1,
+        createdon:1,
+        createdby:1,
+        office:1,
+        body:"i don't agree with final votes",
+    },
+    {
+        id:2,
+        createdon:2,
+        createdby:2,
+        office:2,
+        body:"how can you publish this?",
+    },
+    ];
+    
+    export default potition;
---
+++ db/parties.js
@@ -0,0 +1,17 @@
+const parties = [
+    {
+        id:1,
+        name:"RPF",
+        HQAddress:"Rusororo",
+        logourl:"images/Audio.jpg",
+    },
+    {
+        id:2,
+        name:"pl",
+        HQAddress:"Rusororo",
+        logourl:"images/Aud.jpg",
+    
+    }
+    ];
+    
+export default parties;
