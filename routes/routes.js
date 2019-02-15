18f33c670480897464c5d8558efacac124c9453b
Lukaskov <sabin6341@gmail.com>
2019-02-15 02:17:44 +0200

    update routers

--- routes/routes.js
+++ routes/routes.js
@@ -1,51 +1,51 @@
 import express from "express";
-import postController from "../controller/controller";
-
 const router = express.Router();
-//post routers
-router.get("/api/v1/parties", postController.getparties);
-router.get("/api/v1/user", postController.getuser);
-router.get("/api/v1/office", postController.getoffice);
-router.get("/api/v1/candidates", postController.getcandidates);
-router.get("/api/v1/votes", postController.getvotes);
-router.get("/api/v1/petition", postController.getpetition);
+import partyController from "../controller/partycontroller";
+import officesController from "../controller/officecontroller";
+import usersController from "../controller/usercontroller";
+import candidatesController from "../controller/candidateController"
+import petitionsController from "../controller/petitionController"
+import voteController from "../controller/votesController"
 
 
 //post routers
-router.post("/api/v1/parties", postController.createparties);
-router.post("/api/v1/user", postController.createuser);
-router.post("/api/v1/candidates/", postController.createcandidates);
-router.post("/api/v1/petition/", postController.createpetition);
-router.post("/api/v1/office/", postController.createoffice);
+router.get("/api/v1/parties", partyController.getparties);
+router.get("/api/v1/user", usersController.getuser);
+router.get("/api/v1/office", officesController.getoffice);
+router.get("/api/v1/candidates",candidatesController.getcandidates);
+router.get("/api/v1/votes", voteController.getvotes);
+router.get("/api/v1/petition", petitionsController.getpetition);
 
+//post routers
+router.post("/api/v1/parties", partyController.createparties);
+router.post("/api/v1/user", usersController.createuser);
+router.post("/api/v1/candidates/",candidatesController.createcandidates);
+router.post("/api/v1/petition/", petitionsController.createpetition);
+router.post("/api/v1/office/", officesController.createoffice);
+router.post("/api/v1/votes/", voteController. createvote);
 
-
-
-//get by id data routers
-router.get("/api/v1/user/:id", postController.getOneuser);
-router.get("/api/v1/parties/:id", postController.getOneparty);
-router.get("/api/v1/office/:id", postController.getOneoffice);
-router.get("/api/v1/candidates/:id", postController.getOnecandidate);
-router.get("/api/v1/votes/:id", postController.getOnevote);
-router.get("/api/v1/petition/:id", postController.getOnepetition);
+//get by id  routers
+router.get("/api/v1/user/:id", usersController.getOneuser);
+router.get("/api/v1/parties/:id", partyController.getOneparty);
+router.get("/api/v1/office/:id", officesController.getOneoffice);
+router.get("/api/v1/candidates/:id", candidatesController.getOnecandidate);
+router.get("/api/v1/votes/:id", voteController.getOnevote);
+router.get("/api/v1/petition/:id",petitionsController.getOnepetition);
 
 //updates routers
-router.put("/api/v1/parties/:id", postController.updateparty);
-router.put("/api/v1/office/:id", postController.updateoffice);
-router.put("/api/v1/user/:id", postController.updateuser);
+router.put("/api/v1/parties/:id",partyController.updateparty);
+router.put("/api/v1/office/:id",officesController.updateoffice);
+router.put("/api/v1/user/:id", usersController.updateuser);
 
 
-//updates patch router
-router.patch("/api/v1/parties/:id/:name", postController.updatepartyname);
-
-
-
+// patch router
+router.patch("/api/v1/parties/:id/:name", partyController.updatepartyname);
 
 //delete routers
-router.delete("/api/v1/parties/:id", postController.deleteparties);
-router.delete("/api/v1/user/:id", postController.deleteuser);
-router.delete("/api/v1/candidates/:id", postController.deletecandidate);
-router.delete("/api/v1/office/:id", postController.deleteoffice);
-router.delete("/api/v1/petition/:id", postController.deletepetition);
+router.delete("/api/v1/parties/:id", partyController.deleteparties);
+router.delete("/api/v1/user/:id", usersController.deleteuser);
+router.delete("/api/v1/candidates/:id", candidatesController.deletecandidate);
+router.delete("/api/v1/office/:id",officesController.deleteoffice);
+router.delete("/api/v1/petition/:id", petitionsController.deletepetition);
 
 export default router;
